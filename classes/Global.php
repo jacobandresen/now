83df1ed47e3f6461d463ac22c3c3234006e399c1
Jacob Andresen <jacobandresen@0x50a43e6e.boanxx15.dynamic.dsl.tele.dk>
2009-01-02 22:39:08 +0100

    mentioning php in README

--- classes/Global.php
+++ classes/Global.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect("localhost", "yourusername", "yourpassword") or die(mysql_error());
+mysql_connect("localhost", "jacob", "sockmonkey") or die(mysql_error());
 mysql_select_db("search") or die(mysql_error());
 ?>
