0b0e22f0b0920fef819fedc44adf727cbb34666b
Jacob Andresen <jacob@tatooine.(none)>
2009-01-03 16:37:41 +0100

    using HTTPClient

--- service/pedant.dk/configure.php
+++ service/pedant.dk/configure.php
@@ -1,5 +1,6 @@
 <?php
  require_once('../../classes/Global.php'); 
+ require_once('../../classes/HTTPClient.php'); 
  require_once('../../classes/Search/Framework.php');
 
  print "configure pedant.dk\r\n";
--- service/pedant.dk/run.php
+++ service/pedant.dk/run.php
@@ -1,5 +1,6 @@
 <?php
  require_once('../../classes/Global.php'); 
+ require_once('../../classes/HTTPClient.php'); 
  require_once('../../classes/Search/Framework.php');
 
  $s=new Yas("pedant_dk");
