7ed07875168fc69943e91f56da74a0b547399f40
Jacob Andresen <jacob@tatooine.(none)>
2009-01-18 13:40:49 +0100

    working on admin gui

---
+++ include/Head.php
@@ -0,0 +1,12 @@
+<?php
+require_once('classes/Search/Framework.php');
+require_once('classes/Global.php');
+?>
+<html>
+ <head>
+ <script src="js/mootools-nc.js"></script>
+ </head>
+<body>
+<?php
+ include("LoginCheck.php");
+?>
---
+++ include/LoginCheck.php
@@ -0,0 +1,37 @@
+<?php
+ if(!($yas->LoggedIn())){
+?>
+<form action="index.php" method="post">
+<table>
+<tr>
+ <td colspan="2" align="center">
+  <b>please login</b>
+  </td>
+</tr>
+<tr>
+ <td>
+   login
+ </td> 
+ <td>
+  <input type="text" name="login"/>
+ </td>
+</tr>
+<tr>
+ <td>
+   password 
+ </td>
+ <td>
+  <input type="password" name="password"/>
+ </td>
+</tr>
+<tr>
+ <td>&nbsp;</td>
+ <td align="right">
+   <input type="submit" value="login"/>
+ <td>
+</tr>
+</form>
+</table>
+<?php
+}
+?>
