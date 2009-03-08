81eb5b9b4df7ed1bef413a2dc2f8ff55a955b1d3
Jacob Andresen <jacob.andresen@gmail.com>
2009-03-08 09:01:44 +0100

    store

--- classes/Store.php
+++ classes/Store.php
@@ -1,36 +1,55 @@
 <?php
+
+include('Field.php');
+
 class Store {
 	
-  public  $iOwner; //	
-  public  $sTable;
+  protected  $iOwner; 	
+  protected  $sTable;
 
-
-  function __construct($iOwner){
-     $this->iOwner=$iOwner;
-  }
-
-
-  function Search($sQuery){
+  function __construct($iOwner,$sTable){
+	  $this->iOwner=$iOwner;
+	  $this->sTable=$sTable;
   }
 
   function GetAll(){
+    $results=array();  
+    $res = mysql_query("select id,name,value from ".$this->sTable." where owner='".$this->iOwner."'") or  die (mysql_error());
+    while ($row = mysql_fetch_array($res) ){
+	    $f=new Field();
+	    $f->name= $row['name'];
+	    $f->value= $row['value'];
+            array_push($results, $f);	
+	   }
+    return($results);
   } 
   
   function Get($iID){
+   $f=new Field();
+   $res = mysql_query("select id,name,value from ".$this->sTable." where owner='".$this->iOwnerId."'");
+    if($row=mysql_fetch_array($res)){
+
+       $f 		= new Field();
+       $f->name 	= $row['name'];
+       $f->value 	= $row['value'];
+       $f->type 	= $row['type']; 
+    }
+    return($f); 
    }
 
-  function Put($iID, $sName, $sValue){
+  function Put($sName, $sValue ){
+	  mysql_query("INSERT INTO ".$this->sTable."(owner,name,value) value('".$this->iOwner."','".$sName."','".$sValue."')") or die (mysql_error());
+  
   } 
 
   function Delete($iID){
-
+	mysql_query("DELETE FROM ".$this->sTable." where id='".$iID) or die(mysql_error());
   }
 
+  function DeleteAll(){
+	mysql_query("DELETE FROM ".$this->sTable." where owner='".$this->iOwner."'") or die(mysql_error());
+  }
 };
-
-
-$s=new Store(1);
-$s->sTable="crawlskip";
 
 
 ?>
