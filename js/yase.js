6c5d340066885928d635f165a459c3a7b7b7f539
Jacob Andresen <jacob@tatooine.(none)>
2009-03-08 22:55:01 +0100

    søg

--- js/yase.js
+++ js/yase.js
@@ -2,7 +2,7 @@
 var yase = {
  base:'/yase/yase.php',
  box:function() {
-   $('#search_box').html('<input type="text" name="query" id="query" value=""> <input type="submit" onclick="yase.result(1);">');
+   $('#search_box').html('<input type="text" name="query" id="query" value=""> <input type="submit" value="s&oslash;g" onclick="yase.result(1);">');
   },
   result:function (page) {
      $('#search_result').load(yase.base, 
