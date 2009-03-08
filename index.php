c2b1936460743fc9afac575e5d2d0608aaec791b
Jacob Andresen <jacob.andresen@gmail.com>
2009-03-08 09:12:05 +0100

    moving samples

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
-<?php
-require_once ("include/Head.php");
+ 
+admin <a href="admin/">administration</a>
 
-if($yas->loggedIn()){
-?>
-<script type="text/javascript" src="js/mootools-nc.js"></script>
-<script type="text/javascript" src="js/SkipTable.js"></script>
+yase demo 
+ <a href="samples/search.html">search page</a>
 
-<b>crawl skip:</b>
-<div id="crawlSkip"> </div>
-
-<b>index skip:</b>
-<div id="indexSkip"> </div>
-
-<script type="text/javascript">
-  var sTicket="1234567890"; 
-  var oCrawlSkip=new SkipTable("data.php","oCrawlSkip", "crawlSkip");
-  oCrawlSkip.update();
-  var oIndexSkip=new SkipTable("data.php", "oIndexSkip", "indexSkip");
-  oIndexSkip.update();
-</script>
-<?php
-}
-include ("include/Bottom.php");
-?>
