8c5b75481fb3accc5fd56cc5b9219d13ad2452ec
Jacob Andresen <jacob@tatooine.(none)>
2009-03-15 15:02:48 +0100

    pedant.dk

---
+++ account/pedant.dk/Crawler.php
@@ -0,0 +1,31 @@
+<?php
+require_once ('../../classes/Global.php');
+require_once ('../../classes/UserManagement.php');
+require_once ('../../classes/HTTPClient.php');
+require_once ('../../classes/Crawler.php');
+
+$u = new UserManagement();
+$iUserId = $u->getUserId("pedant.dk");
+
+$c = new Crawler($iUserId);
+$aFilterSkip = array();
+
+//array_push( $aFilterSkip, "print");
+array_push( $aFilterSkip, "\.pdf");
+array_push( $aFilterSkip, "\.ppt");
+array_push( $aFilterSkip, "\.jpeg");
+array_push( $aFilterSkip, "\.jpg");
+array_push( $aFilterSkip, "\.gif");
+array_push( $aFilterSkip, "\.zip");
+array_push( $aFilterSkip, "feed");
+array_push( $aFilterSkip, "\.css");
+array_push( $aFilterSkip, "\.xml");
+array_push( $aFilterSkip, "xmlrpc");
+array_push( $aFilterSkip, "\#respond");
+array_push( $aFilterSkip, "\#comment");
+array_push( $aFilterSkip, "\.war");
+
+$c->aFilterSkip = $aFilterSkip;
+$c->crawler("http://pedant.dk", 0, "http://pedant.dk");
+
+?>
