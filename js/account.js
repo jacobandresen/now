900d6c95ee8ca2847665e293169f6975be9b0ff7
jacob <jacob@searchServer.search>
2009-07-25 15:34:04 +0200

    update to extjs 3.0

---
+++ js/account.js
@@ -0,0 +1,9 @@
+/*
+$(document).ready(function() {
+  //update session when a new account is selected
+  $('#accountSelector').select( function() {
+     //AJAX call to set account
+    alert('hello world'); 
+   });
+ });
+*/
