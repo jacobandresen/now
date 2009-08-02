b39c3f57e8677bffbf31c1ed7ce262bc7eeb2e7f
jacob <jacob@searchServer.search>
2009-08-02 12:10:23 +0200

    account selector

--- js/account.js
+++
@@ -1,9 +0,0 @@
-/*
-$(document).ready(function() {
-  //update session when a new account is selected
-  $('#accountSelector').select( function() {
-     //AJAX call to set account
-    alert('hello world'); 
-   });
- });
-*/
