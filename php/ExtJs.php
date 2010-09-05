546b83e1faca1454050961cbb50c4fa4c162b248
Jacob Andresen <jacob.andresen@gmail.com>
2010-09-05 18:02:51 +0200

    fixing locations

--- php/ExtJs.php
+++ php/ExtJs.php
@@ -7,7 +7,7 @@ class ExtJs
 
     print "  <link rel=\"stylesheet\" type=\"text/css\" href=\"$cacheFly/resources/css/ext-all.css\"/>\r\n";
     print "  <script type=\"text/javascript\" src=\"$cacheFly/adapter/ext/ext-base.js\"></script>\r\n";
-    print "  <script type=\"text/javascript\" src=\"$cacheFly/ext-3.2.1/ext-all-debug.js\"></script>\r\n";
+    print "  <script type=\"text/javascript\" src=\"$cacheFly/ext-all-debug.js\"></script>\r\n";
   }
   
   public static function includeDirectory ($directory)
@@ -17,7 +17,7 @@ class ExtJs
        while (false !== ($file = readdir($handle)))
        {
           if ($file != "." && $file != ".." && (strpos($file, "xds") === false))  
-            print "  <script type=\"text/javascript\" src=\"".$file."\"></script>\r\n";
+            print "  <script type=\"text/javascript\" src=\"".$directory."/".$file."\"></script>\r\n";
        } 
     }
   }
