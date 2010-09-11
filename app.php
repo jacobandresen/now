15bdbbceb299f509643d7c549c8646591277036b
Jacob Andresen <jacob.andresen@gmail.com>
2010-09-11 19:41:24 +0200

    error handling

--- app.php
+++ app.php
@@ -5,6 +5,10 @@ $controller  	= $_REQUEST['controller'];
 $json   	= $_REQUEST['json'];
 $action 	= $_REQUEST['action'];
 
-$app = new JSONApplication();
-print $app->dispatch($controller, $action, $json);
+try{
+  $app = new JSONApplication();
+  print $app->dispatch($controller, $action, $json);
+}catch (Exception $e) {
+  print "json call failed :".$e->getMesage()."\r\n";
+}
 ?>
