d23ed3b085bb82c31237af04aa8185b979d26f8e
Jacob Andresen <jacob.andresen@gmail.com>
2010-09-12 17:48:47 +0200

    adding basic test suite

--- app.php
+++ app.php
@@ -6,11 +6,11 @@ $controller  	= $_REQUEST['controller'];
 $json   	= $_REQUEST['json'];
 $action 	= $_REQUEST['action'];
 
-
 try{
   $app = new JSONApplication();
   print $app->dispatch($controller, $action, $json);
 }catch (Exception $e) {
   print "json call failed :".$e->getMesage()."\r\n";
 }
+
 ?>
---
+++ test/BasicTestSuite.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once '../php/Framework.php';
+
+require_once 'AccountTest.php';
+require_once 'CollectionTest.php';
+require_once 'ControllerTest.php';
+require_once 'CrawlerTest.php';
+require_once 'IndexerTest.php';
+require_once 'SearcherTest.php';
+
+class BasicTestSuite extends PHPUnit_Framework_TestSuite
+{
+   public static function suite()
+   {
+     $suite = new PHPUnit_Framework_TestSuite();
+
+     $suite->addTest(new AccountTest("testCreate"));
+     $suite->addTest(new AccountTest("testDelete"));
+
+     $suite->addTest(new CollectionTest("testCollectionDomains"));
+     $suite->addtest(new CollectionTest("testgetDomains"));
+
+     $suite->addTest(new ControllerTest("testCollectionRetrieve"));
+     
+     $suite->addTest(new CrawlerTest("testCrawl"));
+
+     $suite->addTest(new IndexerTest("testIndex"));
+
+     $suite->addTest(new SearcherTest("testSearch"));
+
+     return $suite; 
+  }
+}
+?>
