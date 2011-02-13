f44d554d6894b335c04c950b119bc1ddd4f4222b
Jacob Andresen <jacob.andresen@gmail.com>
2011-02-13 10:06:16 +0100

    fixing viewport

--- web/js/main.js
+++ web/js/main.js
@@ -1,12 +1,21 @@
-var loginWindow = new YASE.LoginWindow({tokenUrl:'token.php'});
-var token = "";
 
-var viewPort = new Ext.ViewPort({
-
+var viewport = new Ext.Viewport({
+    token: "", 
+    layout: 'border',
+    items: [{
+        region:'menu',
+        title:'north'
+    }, 
+    {
+        region:'center',
+        title:'list' 
+    }]
 });
 
+var loginWindow = new YASE.LoginWindow({tokenUrl:'token.php'});
+
 loginWindow.on('login',  function (token) {
-    token = token;
+    viewport.token = token;
     loginWindow.close();
 });
 
