ebcee24934843a3b6a4974a93f8bdcb50dc8863c
Jacob Andresen <jacob.andresen@gmail.com>
2011-06-13 13:25:58 +0200

    hacking

--- cli/clear.php
+++ cli/clear.php
@@ -2,9 +2,12 @@
 //2011, Jacob Andresen <jacob.andresen@gmail.com>
 require_once("configuration.php");
 require_once("YASE/Framework.php");
+
 mysql_query("delete from collection_domain") or die(mysql_error());
 mysql_query("delete from document") or die(mysql_error());
 mysql_query("delete from collection") or die(mysql_error());
-mysql_query("delete from account") or die (mysql_error());
+mysql_query("delete from field") or die(mysql_error());
 mysql_query("delete from token") or die(mysql_error());
+mysql_query("delete from account") or die (mysql_error());
+
 ?>
--- cli/search.php
+++ cli/search.php
@@ -11,6 +11,10 @@ if (sizeof($argv) < 4 || $argv[0] == "" || $argv[1] == "") {
 }
 
 $account = Account::login($argv[1], $argv[2]);
+
+print_r ($account->collections[0]);
+
 $s = new Searcher($account->collections[0]);
-print_r($s->search($argv[3], 0));
+print_r($s->search($argv[3],0));
+
 ?>
