1740290682f233be8964073f4d6485453b6cef72
Jacob Andresen <jacob.andresen@gmail.com>
2012-01-28 10:31:37 +0100

    hacking

--- php/DAO.php
+++ php/DAO.php
@@ -1,10 +1,33 @@
 <?php 
-class AccountDAO extends DAO
+
+/**
+ * DAO: Data Access Object
+ *
+ * Table name will be mapped like this:
+ *   AccountDAO   -> account
+ *
+ * id's will be mapped like this:
+ *   account->id  ->  account_id
+ * 
+ * attributes will be mapped like this:
+ *   user_name    -> userName
+ *   last_name    -> lastName
+ *
+ * Validation errors will be reported directly to the caller
+ * That means that your calling code should be doing the validation
+ *
+ * 2012, Jacob Andresen <jacob.andresen@gmail.com>
+ * 
+ */
+class DAO
 {
+    //TODO: report database errors
+    //TODO: handle database connections
+
     public function create($json)
     {
-      $account = $this->jsonIn($json);
-      $SQL = $this->generateCreateSQL();
+      $object = json_decode($json);
+      $SQL = $this->generateCreateSQL($object);
       pg_query($dbh, $sql);
     }
 
@@ -16,7 +39,7 @@ class AccountDAO extends DAO
 
     public function update($json)
     {
-      $object = jsonIn(json);
+      $object = json_decode(json);
       $SQL = $this->generateUpdateSQL($object);
       pg_query($dbh, $SQL);
     }
@@ -33,13 +56,13 @@ class AccountDAO extends DAO
       $attributeNames    = $this->getAttributeNames();
 
       $SQL = "INSERT INTO ".$tableName."(";
-      for ($pos = 0 ; $pos < length($SQLAttributeNames)-1; $pos++) {
+      for ($pos = 0 ; $pos < sizeof($SQLAttributeNames)-1; $pos++) {
         $sQL = $SQL.$SQLAttributeNames[$pos].",";
       }
-       $SQL = $SQL.$SQLAttributeNames[$pos].") values(";
+      $SQL = $SQL.$SQLAttributeNames[$pos].") values(";
 
       $attributeNames = $this->getAttributeNames($SQLAttributeNames);
-      for ($pos = 0 ; $pos  < length($attributeNames)-1; $pos++) {
+      for ($pos = 0 ; $pos  < sizeof($attributeNames)-1; $pos++) {
         $SQL = $SQL."'".$object[ $attributeNames[$pos] ]."',";
       }
       $SQL = $SQL."'".$object[ $attributeNames[$pos] ]."')";
@@ -48,17 +71,34 @@ class AccountDAO extends DAO
     
     private function generateRetrieveSQL ($id) {
       $id = pg_escape_string($id);
-      $SQL ="select * from account where account_id='".$id."'";
+
+      $tableName = $this->getTableName();
+
+      //TODO: replace * with attribute names
+      $SQL ="SELECT * FROM account WHERE ".$tableName."_id='".$id."'";
       return $SQL;
     }
 
     private function generateUpdateSQL ($account) {
-      $SQL = "UPDATE ACCOUNT where account_id='".$account->id."' set user_name='".$account->userName."',password='".$account->password."',first_name='".$account->firstName."', last_name='".$account->lastName."'";
+      $tableName         = $this->getTableName(); 
+      $SQLAttributeNames = $this->getSQLAttributeNames();
+      $attributeNames    = $this->getAttributeNames();
+      
+      $SQL = "UPDATE ".$tableName." WHERE ".$tableName."_id SET "
+      
+      for ($pos = 0 ; $pos<sizeof($SQLAttributeNames)-1; $pos++) {
+        $SQL = $SQL.$SQLAttributeNames[$pos]."=".$attributeNames[$pos].",";
+      }  
+      $SQL = $SQL.$SQLAttributeNames[$pos]."=".$attributeNames[$pos];
+        
       return $SQL;
     }
 
     private function generateDestroySQL ($id) {
-      $SQL = "DELETE FROM ACCOUNT where account_id='".$id."'";
+      $id = pg_escape_string($id);
+      $tableName = $this->getTableName();
+
+      $SQL = "DELETE FROM ".$tableName." WHERE ".$tableName."_id='".$id."'";
       return $SQL;
     }
 
@@ -72,18 +112,10 @@ class AccountDAO extends DAO
       $row = pg_fetch_array($result));
 
       $object = new get_class(); 
-      for ($pos=0; $pos < $array_length(attributeNames); $pos ++){
+      for ($pos=0; $pos < $sizeof(attributeNames); $pos ++){
         $object[ $attributeNames[$pos] ] = $row[0]; 
       }
-      return $account;
-    }
-
-    private  function jsonIn ($json) {
-      $object = json_decode($json);
-      //$account->password  = pg_escapestring($account->password);
-      //$account->firstName = pg_escapestring($account->firstName);
-      //$account->lastName  = pg_escapestring($account->lastName);
-      return $account;
+      return $object;
     }
 
     private function getTableName ( ) {
@@ -94,7 +126,7 @@ class AccountDAO extends DAO
     }
 
     private function getSQLAttributeNames () {
-      $tableName = AccountDAO::getTableName();
+      $tableName = $this->getTableName();
       $SQL = "SELECT attname FROM pg_attribute, pg_type where typnam='".$tableName."' and attrelid = typrelid AND attname NOT IN ('cmin','cmax', 'ctid', 'oid', 'tableoid', 'xmin', 'xmax')";
 
       $result = pg_query($dbh, $sql);
@@ -105,7 +137,7 @@ class AccountDAO extends DAO
       $pos = 0;
 
       while( $row = pg_fetch_array($result)) {
-        $SQAattributename = AccountDAO::translateSQLName($row[0]); 
+        $SQAattributename = $this->translateSQLName($row[0]); 
         $SQLAttributeNames[$pos++] = $SQLAttributeName;
       }
 
@@ -117,7 +149,7 @@ class AccountDAO extends DAO
         $SQLAttributeNames = $this->getSQLAttributeNames();
       } 
     
-      for ($pos = 0; $pos < array_length($SQLAttributeNames); $pos++) {
+      for ($pos = 0; $pos < sizeof($SQLAttributeNames); $pos++) {
         $SQLAttributeName = $SQLAttributeNames[$pos];
         //TODO: camelize $name
         //TODO: remember that id's should remove tablename (eg account_id is 'id') 
